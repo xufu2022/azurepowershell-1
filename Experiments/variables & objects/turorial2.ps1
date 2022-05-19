e3a052eafd14160a5f4525f718bbb8a71c481b04
fu xu <xufu2022@gmail.com>
2022-05-19 15:25:18 +0100

    vvafiable

--- Experiments/variables & objects/turorial2.ps1
+++ Experiments/variables & objects/turorial2.ps1
@@ -30,3 +30,44 @@ Get-ChildItem env:variablename
 Get-ChildItem Environment::variablename
 
 
+<# PowerShell provides access to environment variables through its environment pro‐
+vider. Providers let you work with data stores (such as the registry, environment vari‐
+ables, and aliases) much as you would access the filesystem #>
+
+# By default, PowerShell creates a drive (called env) that works with the environment
+# provider to let you access environment variables. The environment provider lets you
+# access items in the env: drive as you would any other drive: dir env:\variablename
+# or dir env:variablename. If you want to access the provider directly (rather than go
+# through its drive), you can also type dir Environment::variablename.
+# However, the most common (and easiest) way to work with environment variables is
+# by typing $env:variablename. This works with any provider but is most typically
+# used with environment variables.
+Get-Content env:systemroot
+
+##  type Invoke-CmdScript Scriptname.cmd or Invoke-CmdScript Scriptname.bat
+
+Set-StrictMode -Version 3
+$files = Get-ChildItem
+foreach($file in $files)
+{
+ $owner = (Get-Acl $file).Owner
+ $file | Add-Member NoteProperty Owner $owner
+ $file
+}
+
+##  Create and Initialize Custom Objects
+### Creating a custom object
+$output = [PSCustomObject] @{
+    'User' = 'DOMAIN\User';
+    'Quota' = 100MB;
+    'ReportDate' = Get-Date;
+   }
+   $output.User
+
+   $r = [PSCustomObject] @{
+    Name = "Lee";
+    Phone = "555-1212";
+    SSN = "123-12-1212"
+   }
+   $r.PSTypeNames.Add("AddressRecord")
+   Update-TypeData -TypeName AddressRecord -MemberType AliasProperty -Membername Cell -Value Phone
