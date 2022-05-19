7af4dd562b51c56fdabba7bf18eca36be093b122
fu xu <xufu2022@gmail.com>
2022-05-19 10:34:25 +0100

    variable

---
+++ Lab - Variables and Data types/script02.ps1
@@ -0,0 +1,20 @@
+$x=10
+$y=20
+'The vaule of x is ' + $x
+"The vaule of x is " + $x
+"The vaule of x is $x" 
+
+$z=$x +$y
+
+"The vaule of x is $z" 
+
+$courseName='Azure powershell'
+
+"then name is $courseName"
+
+# c# language
+$x.GetType()
+$courseName.GetType()
+
+# Methods
+
